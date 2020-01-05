91d2e2dc5122702ce334a973541d85696aaf460f
fugashy <fugashy@icloud.com>
2020-01-05 13:43:36 +0900

    Add robots module

---
+++ src/robots.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+u"""さまざまな2D移動ロボットを表現するクラス
+
+ノイズ無し・ありなど
+"""
+from math import sin, cos
+import matplotlib.patches as patches
+
+class IdealRobot():
+    u"""理想的な2D移動をするロボット"""
+
+    def __init__(self, pose, color):
+        u"""初期位置・色の設定
+
+        Args:
+            pose(np.array): [x(m), y(m), yaw(rad)]
+            color(string): red, green, blue, black, etc...
+        """
+        self.pose = pose
+        self.r = 0.2
+        self.color = color
+
+    def draw(self, ax):
+        u"""描画
+
+        Args:
+            ax(AxesSubplot): 描画オブジェクト
+
+        Returns:
+            なし
+        """
+        x, y, theta = self.pose
+
+        # ロボットの向きを示す線分
+        xn = x + self.r * cos(theta)
+        yn = y + self.r * sin(theta)
+        ax.plot([x, xn], [y, yn], color=self.color)
+
+        c = patches.Circle(
+            xy=(x, y), radius=self.r, fill=False, color=self.color)
+        ax.add_patch(c)
