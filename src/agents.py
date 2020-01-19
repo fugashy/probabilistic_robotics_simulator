b7e5dd63d68687b16ea9e663742181e5ccd87f05
fugashy <fugashy@icloud.com>
2020-01-19 15:24:25 +0900

    Add comments of Agent

--- src/agents.py
+++ src/agents.py
@@ -13,8 +13,26 @@ class Agent():
     """
 
     def __init__(self, nu, omega):
+        u"""速度・角速度(制御指令)の設定
+
+        Args:
+            nu(float): 速度[m/s]
+            omega(float): 角速度[rad/s]
+        """
         self.nu = nu
         self.omega = omega
 
     def decision(self, observation=None):
+        u"""意思決定
+
+        観測があったらそれを考慮するように実装される
+        今は単に，最初に与えられた制御指令をそのまま出力する
+
+        Args:
+            observation((np.array, int)): 観測した位置とID
+
+        Returns:
+            nu(float): 速度[m/s]
+            omega(float): 角速度[rad/s]
+        """
         return self.nu, self.omega
