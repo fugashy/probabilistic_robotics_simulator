0d302b81ce8f6becf2e30e48d2731819823ef92e
fugashy <fugashy@icloud.com>
2020-02-17 14:05:49 +0900

    Add EKF class

--- src/estimators.py
+++ src/estimators.py
@@ -2,13 +2,33 @@
 import copy
 from sys import path
 path.append('../')
-from math import cos, sin, pi, sqrt
+from math import atan2, cos, sin, pi, sqrt
 
+from matplotlib.patches import Ellipse
 import numpy as np
 from scipy.stats import multivariate_normal
 
 import robots
 import sensors
+
+
+def sigma_ellipse(p, cov, n):
+    u"""共分散行列の楕円描画オブジェクトを返す
+
+    Args:
+        p(???):
+        cov(np.ndarray): 共分散行列
+        n(???):
+
+    Returns:
+        matplotlib.patches.Ellipse: 誤差楕円
+    """
+    eigen_values, eigen_vector = np.linalg.eig(cov)
+    angle = atan2(eigen_vector[:, 0][1], eigen_vector[:, 0][0]) / pi * 180.
+    return Ellipse(
+        p, width=2*n*sqrt(eigen_values[0]), height=2*n*sqrt(eigen_values[1]),
+        angle=angle, fill=False, color='blue', alpha=0.5)
+
 
 class Particle():
     def __init__(self, init_pose, weight):
@@ -181,3 +201,60 @@ class Mcl():
         i = np.argmax([p.weight for p in self.particles])
         self.ml = self.particles[i]
         self.pose = self.ml.pose
+
+
+class ExtendedKalmanFilter():
+    u"""拡張カルマンフィルタを用いて自己位置を更新するクラス"""
+
+    def __init__(
+            self,
+            map_, init_pose,
+            motion_noise_stds={'nn': 0.19, 'no': 0.001, 'on': 0.13, 'oo': 0.2}):
+        u"""初期設定
+
+        Args:
+            map_(maps.Map): 環境地図
+            init_pose(np.array): 初期位置
+            motion_noise_stds(dict): 並進速度，回転速度2x2=4Dの標準偏差
+        """
+        # 信念分布を表す正規分布
+        self.belief = multivariate_normal(
+            mean=np.array([0., 0., pi/4.]),
+            cov=np.diag([0.1, 0.2, 0.01]))
+        # [x, y, theta]の3次元
+        self.pose = self.belief.mean
+
+    def motion_update(self, nu, omega, time):
+        u"""位置更新
+
+        Args:
+            nu(???): 速度
+            omega(???): 角速度
+            time(float): 前回から経過した時間(sec)
+        """
+        pass
+
+    def observation_update(self, observation):
+        u"""観測結果から位置を更新する
+        Args:
+            observation((np.array, int)): 観測距離と角度，ID
+        """
+        pass
+
+    def draw(self, ax, elems):
+        u"""自己位置を誤差楕円で描画
+
+        Args:
+            ax(matplotlib.axes,_subplots.AxesSubplot): サブプロットオブジェクト
+            elems([matplotlib.XXX]): 描画可能なオブジェクト(Text, PathCollectionなど)
+        """
+        # xyの3シグマ楕円
+        e = sigma_ellipse(self.belief.mean[0:2], self.belief.cov[0:2, 0:2], 3)
+        elems.append(ax.add_patch(e))
+
+        # θ方向の誤差3シグマ
+        x, y, c = self.belief.mean
+        sigma3 = sqrt(self.belief.cov[2, 2]) * 3.
+        xs = [x + cos(c - sigma3), x, x + cos(c + sigma3)]
+        ys = [y + sin(c - sigma3), y, y + sin(c + sigma3)]
+        elems += ax.plot(xs, ys, color='blue', alpha=0.5)
