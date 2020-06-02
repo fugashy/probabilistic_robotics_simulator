d30bf5c12bb10e854eaac845d3c4e5f3de061133
fugashy <fugashy@icloud.com>
2020-06-03 03:28:26 +0900

    estimators: Delete draw method and modify to inherit classes

--- src/estimators.py
+++ src/estimators.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from abc import abstractmethod
 import copy
 from math import cos, sin, pi, sqrt
 
@@ -21,8 +22,37 @@ _MOTION_NOISE_STDDEV = {
 #       'on': 0.127, 'oo': 0.227
 #   }
 
+class Estimator():
+    u"""位置推定インターフェースクラス"""
+    def __init__(self):
+        self._pose = np.array([0., 0., 0.])
 
-class Mcl():
+    @abstractmethod
+    def motion_update(self, nu, omega, time):
+        u"""位置更新
+
+        Args:
+            nu(float): 速度
+            omega(float): 角速度
+            time(float): 前回から経過した時間(sec)
+        """
+        raise NotImplementedError('This is an abstractmethod')
+
+    @abstractmethod
+    def observation_update(self, observation):
+        u"""観測結果から位置を更新する
+        Args:
+            observation((np.array, int)): 観測距離と角度，ID
+        """
+        raise NotImplementedError('This is an abstractmethod')
+
+    @property
+    def pose(self):
+        u"""推定器としてアウトプットする位置姿勢"""
+        return self._pose
+
+
+class Mcl(Estimator):
     def __init__(
             self,
             map_, init_pose, num,
@@ -38,13 +68,14 @@ class Mcl():
             distance_dev_rate(float): 観測した距離に比例するばらつき
             direction_dev(float): 観測した角度のばらつき
         """
-        self.map = map_
+        super().__init__()
+        self._map = map_
         # 初期の重みは等価値
         # 総和が1になればスケールは変わらない
         initial_weight = 1. / num
-        self.particles = \
+        self._particles = \
             [
-                particles.Particle(init_pose, initial_weight)
+                particles.SimpleParticle(init_pose, initial_weight)
                 for i in range(num)
             ]
 
@@ -57,66 +88,42 @@ class Mcl():
         """
         v = motion_noise_stds
         c = np.diag([v['nn']**2, v['no']**2, v['on']**2, v['oo']**2])
-        self.motion_noise_rate_pdf = multivariate_normal(cov=c)
+        self._motion_noise_rate_pdf = multivariate_normal(cov=c)
 
-        self.distance_dev_rate = distance_dev_rate
-        self.direction_dev = direction_dev
+        self._distance_dev_rate = distance_dev_rate
+        self._direction_dev = direction_dev
 
         # 尤度が最大のパーティクルを代表値とする
         # 初期では適当に先頭
-        self.ml = self.particles[0]
-        self.pose = self.ml.pose
+        self._ml = self._particles[0]
+        self._pose = self._ml.pose
 
     def motion_update(self, nu, omega, time):
         u"""パーティクルを動かす"""
-        for p in self.particles:
-            p.motion_update(nu, omega, time, self.motion_noise_rate_pdf)
-
-
-    def draw(self, ax, elems):
-        u"""パーティクルを矢印として表示
-
-        Args:
-            ax(matplotlib.axes,_subplots.AxesSubplot): サブプロットオブジェクト
-            elems([matplotlib.XXX]): 描画可能なオブジェクト(Text, PathCollectionなど)
-        """
-        xs = [p.pose[0] for p in self.particles]
-        ys = [p.pose[1] for p in self.particles]
-        # パーティクルの重みに比例した長さの矢印を描くため
-        vxs = \
-            [
-                cos(p.pose[2]) * p.weight * len(self.particles)
-                for p in self.particles
-            ]
-        vys = \
-            [
-                sin(p.pose[2]) * p.weight * len(self.particles)
-                for p in self.particles
-            ]
-        elems.append(ax.quiver(xs, ys, vxs, vys,
-            angles='xy', scale_units='xy', scale=1.5, color='blue', alpha=0.5))
+        for p in self._particles:
+            p.motion_update(nu, omega, time, self._motion_noise_rate_pdf)
 
     def observation_update(self, observation):
-        for p in self.particles:
+        for p in self._particles:
             p.observation_update(
                 observation,
-                self.map,
-                self.distance_dev_rate,
-                self.direction_dev)
+                self._map,
+                self._distance_dev_rate,
+                self._direction_dev)
         # リサンプリング後は重みが均一になるので，観測後に最大尤度の姿勢は保持する
-        self.set_ml()
-        self.resampling()
+        self._set_ml()
+        self._resampling()
 
-    def resampling(self):
+    def _resampling(self):
         u"""系統サンプリングを行う"""
         # 重みを累積ベクトル
         # 値の大きさは気にせず，順番に足し合わせる
-        ws = np.cumsum([e.weight for e in self.particles])
+        ws = np.cumsum([e.weight for e in self._particles])
 
         if ws[-1] < 1e-100:
             ws = [e + 1e-100 for e in ws]
 
-        step = ws[-1] / len(self.particles)
+        step = ws[-1] / len(self._particles)
 
         r = np.random.uniform(0.0, step)
 
@@ -124,22 +131,22 @@ class Mcl():
 
         ps = []
 
-        while len(ps) < len(self.particles):
+        while len(ps) < len(self._particles):
             if r < ws[cur_pos]:
-                ps.append(self.particles[cur_pos])
+                ps.append(self._particles[cur_pos])
                 r += step
             else:
                 cur_pos += 1
 
-        self.particles = [copy.deepcopy(e) for e in ps]
-        for p in self.particles:
-            p.weight = 1.0 / len(self.particles)
+        self._particles = [copy.deepcopy(e) for e in ps]
+        for p in self._particles:
+            p.weight = 1.0 / len(self._particles)
 
-    def set_ml(self):
+    def _set_ml(self):
         u"""最大尤度のパーティクルを推定機としての出力する姿勢とする"""
-        i = np.argmax([p.weight for p in self.particles])
-        self.ml = self.particles[i]
-        self.pose = self.ml.pose
+        i = np.argmax([p.weight for p in self._particles])
+        self._ml = self._particles[i]
+        self._pose = self._ml.pose
 
 
 class FastSlam(Mcl):
@@ -161,28 +168,24 @@ class FastSlam(Mcl):
             None, init_pose, particle_num, motion_noise_stds,
             distance_dev_rate, direction_dev)
 
-        self.particles = \
+        self._particles = \
             [
                 particles.MapParticle(
                     init_pose, 1. / particle_num, landmark_num)
                 for i in range(particle_num)
             ]
-        self.ml = self.particles[0]
+        self._ml = self._particles[0]
 
     def observation_update(self, observation):
-        for p in self.particles:
+        for p in self._particles:
             p.observation_update(
-                observation, self.distance_dev_rate, self.direction_dev)
+                observation, self._distance_dev_rate, self._direction_dev)
 
-        self.set_ml()
-        self.resampling()
-
-    def draw(self, ax, elems):
-        super().draw(ax, elems)
-        self.ml.map.draw(ax, elems)
+        self._set_ml()
+        self._resampling()
 
 
-class ExtendedKalmanFilter():
+class ExtendedKalmanFilter(Estimator):
     u"""拡張カルマンフィルタを用いて自己位置を更新するクラス"""
 
     def __init__(
@@ -199,74 +202,43 @@ class ExtendedKalmanFilter():
             distance_dev_rate(float): 観測した距離に比例するばらつき
             direction_dev(float): 観測した角度のばらつき
         """
-        self.map = map_
+        self._map = map_
         # 信念分布を表す正規分布
-        self.belief = multivariate_normal(
+        self._belief = multivariate_normal(
             mean=np.array([0., 0., 0.]), cov=np.diag([1e-10, 1e-10, 1e-10]))
-        self.motion_noise_stds = motion_noise_stds
+        self._motion_noise_stds = motion_noise_stds
         # [x, y, theta]の3次元
-        self.pose = self.belief.mean
+        self._pose = self._belief.mean
 
-        self.distance_dev_rate = distance_dev_rate
-        self.direction_dev = direction_dev
+        self._distance_dev_rate = distance_dev_rate
+        self._direction_dev = direction_dev
 
     def motion_update(self, nu, omega, time):
-        u"""位置更新
-
-        Args:
-            nu(float): 速度
-            omega(float): 角速度
-            time(float): 前回から経過した時間(sec)
-        """
         # 0割回避
         if abs(omega) < 1e-5:
             omega = 1e-5
 
-        theta = self.belief.mean[2]
+        theta = self._belief.mean[2]
 
-        M = utilities.matM(nu, omega, time, self.motion_noise_stds)
+        M = utilities.matM(nu, omega, time, self._motion_noise_stds)
         A = utilities.matA(nu, omega, time, theta)
         F = utilities.matF(nu, omega, time, theta)
 
-        self.belief.cov = F @ self.belief.cov @ F.T + A @ M @ A.T
-        self.belief.mean = robots.IdealRobot.state_transition(nu, omega, time, self.belief.mean)
-        self.pose = self.belief.mean
-
+        self._belief.cov = F @ self._belief.cov @ F.T + A @ M @ A.T
+        self._belief.mean = robots.IdealRobot.state_transition(nu, omega, time, self._belief.mean)
+        self._pose = self._belief.mean
 
     def observation_update(self, observation):
-        u"""観測結果から位置を更新する
-        Args:
-            observation((np.array, int)): 観測距離と角度，ID
-        """
         for d in observation:
             z = d[0]
             obs_id = d[1]
 
-            H = utilities.matH(self.belief.mean, self.map.landmarks[obs_id].pos)
+            H = utilities.matH(self._belief.mean, self._map.landmarks[obs_id].pos)
             estimated_z = sensors.IdealCamera.observation_function(
-                self.belief.mean, self.map.landmarks[obs_id].pos)
-            Q = utilities.matQ(estimated_z[0] * self.distance_dev_rate, self.direction_dev)
-            K = self.belief.cov @ H.T @ np.linalg.inv(Q + H @ self.belief.cov @ H.T)
+                self._belief.mean, self._map.landmarks[obs_id].pos)
+            Q = utilities.matQ(estimated_z[0] * self._distance_dev_rate, self._direction_dev)
+            K = self._belief.cov @ H.T @ np.linalg.inv(Q + H @ self._belief.cov @ H.T)
 
-            self.belief.mean += K @ (z - estimated_z)
-            self.belief.cov = (np.eye(3) - K @ H) @ self.belief.cov
-            self.pose = self.belief.mean
-
-    def draw(self, ax, elems):
-        u"""自己位置を誤差楕円で描画
-
-        Args:
-            ax(matplotlib.axes,_subplots.AxesSubplot): サブプロットオブジェクト
-            elems([matplotlib.XXX]): 描画可能なオブジェクト(Text, PathCollectionなど)
-        """
-        # xyの3シグマ楕円
-        # スライシング[0:2]でxyのみ抽出
-        e = utilities.sigma_ellipse(self.belief.mean[0:2], self.belief.cov[0:2, 0:2], 3)
-        elems.append(ax.add_patch(e))
-
-        # θ方向の誤差3シグマ
-        x, y, c = self.belief.mean
-        sigma3 = sqrt(self.belief.cov[2, 2]) * 3.
-        xs = [x + cos(c - sigma3), x, x + cos(c + sigma3)]
-        ys = [y + sin(c - sigma3), y, y + sin(c + sigma3)]
-        elems += ax.plot(xs, ys, color='blue', alpha=0.5)
+            self._belief.mean += K @ (z - estimated_z)
+            self._belief.cov = (np.eye(3) - K @ H) @ self._belief.cov
+            self._pose = self._belief.mean
