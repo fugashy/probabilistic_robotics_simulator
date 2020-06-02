9696eb527f800980db4d3d920e39465c92b84255
fugashy <fugashy@icloud.com>
2020-06-03 03:29:34 +0900

    landmarks: Delete draw method and modify to inherit classes

--- src/landmarks.py
+++ src/landmarks.py
@@ -1,50 +1,26 @@
 # -*- coding: utf-8 -*-
 u"""ランドマーク"""
-
+from abc import abstractmethod
 import numpy as np
 
 import utilities
 
 
-class Point2DLandmark:
+class Point2DLandmark():
     u"""2Dの点ランドマーク"""
 
     def __init__(self, x, y):
-        self.pos = np.array([x, y]).T
-        self.id = np.nan
+        self._pos = np.array([x, y]).T
+        self._id = np.nan
 
-    def draw(self, ax, elems):
-        u"""点ランドマークを散布図として描画する
+    def pos(self):
+        return self._pos
 
-        参照で渡されている描画要素elemsを追加するのが目的
-
-        Args:
-            ax(matplotlib.axes,_subplots.AxesSubplot): サブプロットオブジェクト
-            elems([matplotlib.XXX]): 描画可能なオブジェクト(Text, PathCollectionなど)
-        """
-        c = ax.scatter(
-            self.pos[0], self.pos[1],
-            s=100, marker='*', label='landmark', color='orange')
-        elems.append(c)
-        elems.append(
-            ax.text(
-                self.pos[0], self.pos[1],
-                'id:' + str(self.id), fontsize=10))
+    def get_id(self):
+        return self._id
 
 
 class Point2DLandmarkEstimated(Point2DLandmark):
     def __init__(self):
         super().__init__(0., 0.)
-        self.cov = None
-
-    def draw(self, ax, elems):
-        if self.cov is None:
-            return
-
-        c = ax.scatter(
-            self.pos[0], self.pos[1], s=100, marker='*', label='landmarks', color='blue')
-        elems.append(c)
-        elems.append(ax.text(self.pos[0], self.pos[1], 'id:' + str(self.id), fontsize=10))
-
-        e = utilities.sigma_ellipse(self.pos, self.cov, 3)
-        elems.append(ax.add_patch(e))
+        self._cov = None
