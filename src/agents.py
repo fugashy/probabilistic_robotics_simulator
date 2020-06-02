414c1da5402a3c3615e7bbc14737a48b2ce2e900
fugashy <fugashy@icloud.com>
2020-06-02 23:58:22 +0900

    Delete draw method and modify to inherit classes

--- src/agents.py
+++ src/agents.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from abc import abstractmethod
 import math
 u"""エージェントクラス
 
@@ -7,22 +8,14 @@ u"""エージェントクラス
 ロボティクスや人工知能の分野ではそう呼ぶ
 """
 
+
 class Agent():
     u"""考える主体
 
-    観測を受け取り、制御指令を変えす
+    観測を受け取り、制御指令を返す
     """
 
-    def __init__(self, nu, omega):
-        u"""速度・角速度(制御指令)の設定
-
-        Args:
-            nu(float): 速度[m/s]
-            omega(float): 角速度[rad/s]
-        """
-        self.nu = nu
-        self.omega = omega
-
+    @abstractmethod
     def decision(self, observation=None):
         u"""意思決定
 
@@ -36,30 +29,41 @@ class Agent():
             nu(float): 速度[m/s]
             omega(float): 角速度[rad/s]
         """
-        return self.nu, self.omega
+        raise NotImplementedError('This is an abstractmethod')
 
 
-class EstimationAgent(Agent):
+class SimpleAgent(Agent):
+    u"""考えないけどAgent"""
+
+    def __init__(self, nu, omega):
+        u"""速度・角速度(制御指令)の設定
+
+        Args:
+            nu(float): 速度[m/s]
+            omega(float): 角速度[rad/s]
+        """
+        self._nu = nu
+        self._omega = omega
+
+    def decision(self, observation=None):
+        return self._nu, self._omega
+
+
+class EstimationAgent(SimpleAgent):
     def __init__(self, time_interval, nu, omega, estimator):
         super().__init__(nu, omega)
-        self.estimator = estimator
+        self._estimator = estimator
         self.time_interval = time_interval
 
         self.prev_nu = 0.0
         self.prev_omega = 0.0
 
-    def draw(self, ax, elems):
-        self.estimator.draw(ax, elems)
-        x, y, t = self.estimator.pose
-        s = '({:.2f}, {:.2f}, {})'.format(x, y, int(t*180/math.pi)%360)
-        elems.append(ax.text(x, y+0.1, s, fontsize=8))
-
     def decision(self, observation=None):
-        self.estimator.motion_update(
+        self._estimator.motion_update(
             self.prev_nu, self.prev_omega, self.time_interval)
 
-        self.prev_nu, self.prev_omega = self.nu, self.omega
+        self.prev_nu, self.prev_omega = self._nu, self._omega
 
-        self.estimator.observation_update(observation)
+        self._estimator.observation_update(observation)
 
-        return self.nu, self.omega
+        return self._nu, self._omega
